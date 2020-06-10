13c0cb85ff96cd8d877a5ef56be73f71cd4e50d7
Ariel Thompson <ariel.thompson@caci.com>
2020-06-10 12:29:19 -0600

    added printscreen to number layer

--- keyboards/planck/keymaps/ariel/keymap.c
+++ keyboards/planck/keymaps/ariel/keymap.c
@@ -51,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_GRV,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,         KC_MINS,
-    KC_TAB,  KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_RAISE, KC_SCLN),      KC_QUOT,
-    KC_LSPO, LCTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH),KC_RSPC ,
-    KC_EQL,  KC_LCBR,   KC_LBRC, LOWER,  KC_ENT,   KC_BSPC, KC_TAB,  KC_SPC,  RAISE,   KC_RBRC, KC_RCBR,      KC_PLUS
+    KC_GRV,  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                KC_MINS,
+    KC_TAB,  KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_RAISE, KC_SCLN), KC_QUOT,
+    KC_LSPO, LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH),     KC_RSPC ,
+    KC_EQL,  KC_LCBR,      KC_LBRC, LOWER,  KC_ENT,   KC_BSPC, KC_TAB,  KC_SPC,  RAISE,   KC_RBRC, KC_RCBR,             KC_PLUS
 ),
 
 /* Lower
@@ -72,12 +72,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,
-    _______, _______, _______,  LOWER,   _______, _______, _______, _______, RAISE,   KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______,  LOWER,   _______, _______, _______, _______, RAISE,  KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |  Up  |      |      |      |   7  |   8  |   9  |      |      |
+ * |      |      |      |  Up  |      |      |      |   7  |   8  |   9  |      |PrtScr|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      | Right| Down | Right|      |      |   4  |   5  |   6  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -87,10 +87,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    _______, _______, _______, KC_UP, _______, _______, _______, KC_7,  KC_8, KC_9,   _______, _______,
-    _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_4,  KC_5, KC_6,   _______, _______, 
-    _______, _______, _______, _______, _______, _______, _______, KC_1,  KC_2, KC_3,   _______, _______,
-    _______, _______, _______, LOWER,   _______, _______, _______, KC_DOT, RAISE, KC_0, _______, _______
+    _______, _______, _______, KC_UP,    _______, _______, _______, KC_7,  KC_8,   KC_9, _______, KC_PSCR,
+    _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_4,  KC_5,   KC_6, _______, _______, 
+    _______, _______, _______, _______,  _______, _______, _______, KC_1,  KC_2,   KC_3, _______, _______,
+    _______, _______, _______, LOWER,    _______, _______, _______, KC_DOT, RAISE, KC_0, _______, _______
 ),
 
 /* Plover layer (http://opensteno.org)
