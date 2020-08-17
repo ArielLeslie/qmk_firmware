c513f83a7fb04cc7ccd0f7e401eba8352708c116
ArielLeslie <ariellt87@gmail.com>
2020-08-17 15:25:05 -0600

    "move symbols"

--- keyboards/planck/keymaps/ariel/keymap.c
+++ keyboards/planck/keymaps/ariel/keymap.c
@@ -41,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |  `   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |  `   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Shift(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   =  |  {   |  [   | Lower| Enter| Bksp |  Tab |Space | Raise|   ]  |   }  |  +   |
+ * |   +  |  {   |  [   | Lower| Enter| Bksp |  Tab |Space | Raise|   ]  |   }  |  -   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_GRV,  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                KC_MINS,
-    KC_TAB,  KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_RAISE, KC_SCLN), KC_QUOT,
+    KC_GRV,  KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                KC_BSLS,
+    KC_TAB,  KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(_RAISE, KC_SCLN), LT(KC_APP, KC_QUOT),
     KC_LSPO, LCTL_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH),     KC_RSPC ,
-    KC_EQL,  KC_LCBR,      KC_LBRC, LOWER,  KC_ENT,   KC_BSPC, KC_TAB,  KC_SPC,  RAISE,   KC_RBRC, KC_RCBR,             KC_PLUS
+    KC_PLUS,  KC_LCBR,      KC_LBRC, LOWER,  KC_ENT,   KC_BSPC, KC_TAB,  KC_SPC,  RAISE,   KC_RBRC, KC_RCBR,            KC_MINS
 ),
 
 /* Lower
